546e84dfaa1273e16dd22a1cbccb01c0f8fa8a9a
abhinay <palleabhinay462@gmail.com>
2026-06-12 10:40:35 +0530

    commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -60,7 +60,7 @@ export function Header() {
           </div>               
         </div>
 
-        {/* Mobile Navigation */}
+        {/* Mobile Navigation */}                
         {isMenuOpen && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 border-t border-gray-200">
